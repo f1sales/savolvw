533c928688b9ed3df56e336322e9c6d92ec4aff6
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-25 15:31:59 -0300

    Remove Savol Kia test, there is already a gem for Savol Kia

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,44 +3,6 @@ require 'f1sales_custom/parser'
 require 'f1sales_custom/source'
 
 RSpec.describe F1SalesCustom::Email::Parser do
-  context 'when is a different template' do
-    let(:email) do
-      email = OpenStruct.new
-      email.to = [email: 'website@savolkia.f1sales.org']
-      email.subject = 'OKM - Stinger'
-      email.body = "Veículo: StingerNome: Marcio KlepaczE-mail: marcioklepacz@gmail.comCPF: 276.386.810-02Telefone: 11981587311Mensagem: Teste lead"
-
-      email
-    end
-
-    let(:parsed_email) { described_class.new(email).parse }
-
-    it 'contains lead website a source name' do
-      expect(parsed_email[:source][:name]).to eq(F1SalesCustom::Email::Source.all[1][:name])
-    end
-
-    it 'contains name' do
-      expect(parsed_email[:customer][:name]).to eq('Marcio Klepacz')
-    end
-
-    it 'contains email' do
-      expect(parsed_email[:customer][:email]).to eq('marcioklepacz@gmail.com')
-    end
-
-    it 'contains phone' do
-      expect(parsed_email[:customer][:phone]).to eq('11981587311')
-    end
-
-    it 'contains message' do
-      expect(parsed_email[:message]).to eq('Teste lead')
-    end
-
-    it 'contains product' do
-      expect(parsed_email[:product][:name]).to eq('OKM - Stinger')
-    end
-
-  end
-
   context 'when is from website to SBC' do
     let(:email) do
       email = OpenStruct.new
